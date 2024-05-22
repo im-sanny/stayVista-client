a5e12b1793f188b37333ffdcf270270aaa6fed33
im-sanny <raselparvezsanny@gmail.com>
2024-05-22 16:00:03 +0600

    added guest, host, admin, and common dashboard pages

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,17 +1,21 @@
 import { Outlet } from "react-router-dom";
-
+import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
 
 const DashboardLayout = () => {
-    return (
-        <div>
-            {/* sidebar */}
-            <div className="min-h-screen ">sidebar</div>
-            {/* outlet, dynamic content */}
-            <div className="">
-                <Outlet></Outlet>
-            </div>
+  return (
+    <div>
+      {/* sidebar */}
+      <div className="relative md:flex min-h-screen ">
+        <Sidebar></Sidebar>
+      </div>
+      {/* outlet, dynamic content */}
+      <div className="flex-1 md:ml-64">
+        <div className="p-5">
+          <Outlet></Outlet>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
